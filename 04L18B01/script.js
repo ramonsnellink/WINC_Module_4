a2dd8af930135bc0e4f15db8876f4d699f904650
Ramon Snellink <r.snellink@gmail.com>
2020-09-30 11:00:00 +0200

    lvl 18 assignment 1

---
+++ 04L18B01/script.js
@@ -0,0 +1,32 @@
+// zet de functie om naar een arrow functie
+// Deel A
+
+const ikRockArrowFunctions = () => {
+  console.log("Joe, ik rock de arrow functions!");
+};
+
+ikRockArrowFunctions();
+
+// Deel B
+
+const fivePlusSeven = () => 5 + 7;
+
+console.log(fivePlusSeven());
+
+// Deel C
+
+const myFunction = (a, b) => a + b;
+
+console.log(myFunction(5, 6));
+
+// Deel D
+
+const addFive = (a) => a + 5;
+
+console.log(addFive(10));
+
+// Deel E
+
+const createObject = () => ({ greeting: "hoi" });
+
+console.log(createObject());
