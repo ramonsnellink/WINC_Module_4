5211d72e797b9550dff2acc3a69a96db7b08c068
Ramon Snellink <r.snellink@gmail.com>
2020-10-01 13:43:54 +0200

    first version of level 19 assignment 1

---
+++ 04L19B01/script.js
@@ -0,0 +1,42 @@
+const buttons = document.querySelectorAll(".big-five-button");
+const spottedAnimalList = document.querySelector("#spotted-animals-list");
+const removeFirstItemButton = document.querySelector("#remove-first-item-button");
+const removeAllItemsButton = document.querySelector("#remove-all-button");
+
+//Add Item to the list of spotted animals
+
+buttons.forEach((button) => {
+  const animalName = button.innerHTML;
+
+  button.addEventListener("click", () => {
+    const spottedListItem = document.createElement("li");
+
+    spottedAnimalList.appendChild(spottedListItem);
+    spottedListItem.classList.add("spotted-animals-list-item");
+    spottedListItem.innerHTML = animalName;
+  });
+});
+
+//remove the first item of the spotted item list
+
+removeFirstItemButton.addEventListener("click", () => {
+  const firstChild = spottedAnimalList.getElementsByClassName("spotted-animals-list-item")[0];
+
+  spottedAnimalList.removeChild(firstChild);
+});
+
+// Remove all items from the list
+
+removeAllItemsButton.addEventListener("click", () => {
+  //spottedAnimalList
+  const children = spottedAnimalList.getElementsByClassName("spotted-animals-list-item");
+
+  console.log(children);
+  console.log(spottedAnimalList);
+
+  // FIXEN VANAF HIER.
+
+  children.forEach((child) => {
+    spottedAnimalList.removeChild(child);
+  });
+});
