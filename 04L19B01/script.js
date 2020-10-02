d7d28961f77e69820f28b60aa6dbe811b62af0bb
Ramon Snellink <r.snellink@gmail.com>
2020-10-02 20:20:36 +0200

    not fixed yet

--- 04L19B01/script.js
+++ 04L19B01/script.js
@@ -1,5 +1,6 @@
 const buttons = document.querySelectorAll(".big-five-button");
 const spottedAnimalList = document.querySelector("#spotted-animals-list");
+const spottedAnimalListItem = document.querySelectorAll(".spotted-animals-list-item");
 const removeFirstItemButton = document.querySelector("#remove-first-item-button");
 const removeAllItemsButton = document.querySelector("#remove-all-button");
 
@@ -28,20 +29,6 @@ removeFirstItemButton.addEventListener("click", () => {
 // Remove all items from the list
 
 removeAllItemsButton.addEventListener("click", () => {
-  //spottedAnimalList
-  const animal = spottedAnimalList.getElementsByClassName("spotted-animals-list-item");
 
-  console.log(children);
-  console.log(spottedAnimalList);
 
-  // FIXEN VANAF HIER.
-
-  //   window.emptyList = function () {
-  //     var ul = document.querySelector('.list');
-  //     var listLength = ul.children.length;
-
-  //     for (i = 0; i < listLength; i++) {
-  //       ul.removeChild(ul.children[0]);
-  //     }
-  //   }
 });
